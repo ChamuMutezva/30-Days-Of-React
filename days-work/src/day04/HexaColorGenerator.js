fdcbc95168508387b90405a904e776dd3f82e045
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-10-11 09:44:33 +0200

    feat: number generator function complete

--- days-work/src/day04/HexaColorGenerator.js
+++ days-work/src/day04/HexaColorGenerator.js
@@ -1,18 +1,18 @@
 import React from 'react'
 // Hexadecimal color generator
-const hexaColor = () => {  
-  let str = '0123456789abcdef' ;
-  console.log() 
-    let color = ''
-    for (let i = 0; i < 6; i++) {
-      let index = Math.floor(Math.random() * str.length)
-      color += str[index]
-    }
-    return '#' + color 
+const hexaColor = () => {
+  let str = '0123456789abcdef';
+  console.log()
+  let color = ''
+  for (let i = 0; i < 6; i++) {
+    let index = Math.floor(Math.random() * str.length)
+    color += str[index]
   }
-  
-const HexaColor = () => { 
- 
-return <div style={{ backgroundColor: hexaColor(), width: '90%', height: '50px', margin: '5px auto', lineHeight: '50px' }}>{hexaColor() }</div>
+  return '#' + color
+}
+
+const HexaColor = () => {
+
+  return <div style={{ backgroundColor: hexaColor(), width: '90%', height: '50px', margin: '5px auto', lineHeight: '50px' }}>{hexaColor()}</div>
 }
 export default HexaColor
--- days-work/src/day06/numberGenerator.js
+++ days-work/src/day06/numberGenerator.js
@@ -1,30 +1,49 @@
 import React from 'react'
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 const isPrime = num => {
+    let count = 0;
     for (let i = 2; i < num; i++) {
-        if(num % i === 0) {
+        if (num % i === 0) {
+            count++;
             return false;
-        } else {
-        return true
         }
-    }   
-   
+    }
+
+    if (count === 0) {
+        console.log("red")
+        return true
+    }
+
+}
+const isEven = (num) => {
+    if (num === 0 || num % 2 === 0) {
+        return "green"
+    }
+}
+const isOne = (num) => {
+    if (num === 1) {
+        return "yellow"
+    }
 }
 const numList = nums.map(num => {
-   // isPrime(num);
-   // const background = isPrime(num) ? "red" : "green"
-   const background = isPrime(num) ? "red" : num % 2 === 0 ? "green" : "yellow"
-  // let background = "yellow"
- 
- 
-return <div key={num} style={{width: "90px", height: "90px",
-textAlign: "center", lineHeight: "90px", backgroundColor: background}}>{num}</div>})
+    const background = isOne(num) ? "yellow" : isEven(num) ? "green" : isPrime(num) ? "red" : "yellow"
+
+
+    return <div key={num} style={{
+        width: "140px", height: "140px", borderBottom: "2px solid white",
+        textAlign: "center", lineHeight: "140px", backgroundColor: background
+    }}>{num}</div>
+})
 const NumberGenerator = () => {
 
     return (
         <>
-            <h1>Number generator</h1>
-            <div className="numbers">
+            <h1 style={{ textAlign: "center" }}>30 Days Of React</h1>
+            <h2 style={{ textAlign: "center" }}>Number generator</h2>
+            <div className="numbers" style={{
+                display: "flex", flexWrap: "wrap", width: "80%",
+                margin: "auto"
+            }}>
                 {numList}
             </div>
         </>
