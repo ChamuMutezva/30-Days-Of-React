29cc2bebd8a45c0481fcd9815b23c6d3e0fd72b5
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-10-18 10:42:05 +0200

    feat getDerivedStateFromPros

--- days-work/src/App.js
+++ days-work/src/App.js
@@ -1,10 +1,10 @@
 import React from  'react'
-import Main from './day13/main'
+import Main2 from './day14/derivedStates'
 //import FormsPart1 from './day12/formsPart1'
 const App = () => {
     return(
         <>         
-            <Main />
+            <Main2 firstName="Chamu"/>
         </>
     )
 }
