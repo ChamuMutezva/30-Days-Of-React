2b0314663626403aa1d4d2188d49a39b8703fa83
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-10-18 09:03:19 +0200

    feat: uncontolled forms practice - React.createRef()

--- days-work/src/App.js
+++ days-work/src/App.js
@@ -1,10 +1,10 @@
 import React from  'react'
-import Main from './day12/main'
-import FormsPart1 from './day12/formsPart1'
+import Main from './day13/main'
+//import FormsPart1 from './day12/formsPart1'
 const App = () => {
     return(
         <>         
-            <FormsPart1 />
+            <Main />
         </>
     )
 }
